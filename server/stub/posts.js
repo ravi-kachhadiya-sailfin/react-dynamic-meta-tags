24fe729e8c8d2812267dd4b6d3a2d1741ebdd1ba
ravi-kachhadiya-sailfin <95468178+ravi-kachhadiya-sailfin@users.noreply.github.com>
2021-12-03 17:39:25 +0530

    Update posts.js

--- server/stub/posts.js
+++ server/stub/posts.js
@@ -2,7 +2,7 @@ const posts = [
     {
         title: "Post #1",
         description: "This is the first post",
-        thumbnail: "https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80"
+        thumbnail: "https://storage.googleapis.com/pam-images/thumbnail/04cd2fc7-ee74-4153-bc19-31ef2362fda4.jpg"
     },
     {
         title: "Post #2",
