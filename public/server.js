0a950b4b198e51f2051909faa5952a8bbe997a04
akshay91 <akshay91@stanford.edu>
2021-12-06 13:12:45 +0530

    start changes

--- public/server.js
+++ public/server.js
@@ -1,6 +1,6 @@
-const {pathname} = window.location;
+// const {pathname} = window.location;
 
-if (pathname.split('/')[1] === "post") {
-    const meta_title = document.getElementById("meta_title");
-    meta_title.setAttribute("content", "Testing meta title server");
-}
+// if (pathname.split('/')[1] === "post") {
+//     const meta_title = document.getElementById("meta_title");
+//     meta_title.setAttribute("content", "Testing meta title server");
+// }
