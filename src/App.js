82966d649dd22c9852bac051bc7ff293da89317e
akshay91 <akshay91@stanford.edu>
2021-12-06 11:23:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import React, { useState, useEffect } from "react";
 import './App.css';
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 function App() {
   const [mounted, setMounted] = useState(false)
-  const { pathname } = useLocation();
+  const { pathname } = window.location;
 
   if (!mounted) {
     console.log("path:", pathname, pathname.split('/')[1] === "post");
-    if (pathname.split('/')[1] === "probsolve") {
+    if (pathname.split('/')[1] === "post") {
       const meta_title = document.getElementById("meta_title");
       meta_title.setAttribute("content", "Testing meta title")
     }
