2e56830ce7c64c329f425d8d6c6f484a34a09424
akshay91 <akshay91@stanford.edu>
2022-04-25 17:22:42 +0530

    package changes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request')
 const path = require('path');
 const fs = require("fs"); 
 const { getPostById } = require('./stub/posts');
@@ -15,6 +16,49 @@ app.use(express.static(
 
 // here we serve the index.html page
 app.get('/*', (req, res, next) => {
+    console.log("reuq...")
+    // console.log(req);
+    var url1="https://pam-api-uat.med.stanford.edu/";
+    let options={
+        url:url1+ "api/v1/tool/details?toolId=3d5f006c-dcc1-4bd4-99a9-e3adda6fda42",
+        method: "GET"
+    }
+    request(options,(err,data)=>{
+        if(err) {
+            return res.send(err);
+        } else {
+            if(data && data.body){
+                console.log(JSON.parse(data.body))
+                console.log(JSON.parse(data.body).data.tool)
+                data.body = JSON.parse(data.body)
+                fs.readFile(indexPath, 'utf8', (err, htmlData) => {
+                    if (err) {
+                        console.error('Error during file reading', err);
+                        return res.status(404).end()
+                    }
+                    // get post info
+                    const postId = req.query.id;
+                    const post = getPostById(postId);
+                    if(!post) return res.status(404).send("Post not found");
+                    
+                    // inject meta tags
+                    htmlData = htmlData.replace(
+                        "<title>React App</title>",
+                        `<title>${post.title}</title>`
+                    )
+                    .replace('__META_OG_TITLE__', post.title)
+                    .replace('__META_OG_DESCRIPTION__', post.description)
+                    .replace('__META_DESCRIPTION__', post.description)
+                    .replace('__META_OG_IMAGE__', post.thumbnail)
+            
+                    return res.send(htmlData);
+                });
+            }
+        }
+    })
+});
+
+app.get('/posts', (req, res, next) => {
     fs.readFile(indexPath, 'utf8', (err, htmlData) => {
         if (err) {
             console.error('Error during file reading', err);
@@ -38,6 +82,7 @@ app.get('/*', (req, res, next) => {
         return res.send(htmlData);
     });
 });
+
 
 // listening...
 app.listen(PORT, (error) => {
